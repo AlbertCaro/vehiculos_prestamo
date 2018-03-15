3c4b5599ba66a18f557e317d7a44f14d2e9cf6d8
Erick Guerrero <erickg@valles.udg.mx>
2018-03-15 10:45:42 -0600

    Arreglado datepicker

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,7 +12,7 @@
         <link href="{{ asset('font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="{{ asset('css/jquery-ui.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/datetimepicker.css') }}')}}">
+        <link rel="stylesheet" href="{{ asset('css/datetimepicker.css') }}">
         <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
         <script type="text/javascript" src="{{ asset('js/causa.js') }}"></script>
         <script type="text/javascript" src="{{ asset('js/confirmDel.js') }}"></script>
