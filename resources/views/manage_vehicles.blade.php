9c5b1bcba4add1648c6fcf3b4f9c1cfcf1a1ea5b
ignacio <ignaciomenchaca666@gmail.com>
2018-04-13 16:49:24 -0500

    Cambios menores

--- resources/views/manage_vehicles.blade.php
+++ resources/views/manage_vehicles.blade.php
@@ -10,6 +10,7 @@
         <br/>
         @if (session('alert'))
             <div id="message" class="alert alert-success">
+                <a href="#" onclick="fadeMessage()" class="close" title="close">×</a>
                 {{ session('alert') }}
             </div>
         @endif
@@ -39,12 +40,12 @@
                             </a>
                             <input name="_method" type="hidden" value="DELETE">
                             {{ csrf_field() }}
-                            <a href="#" onclick="fadeMessage()" class="close" title="close">×</a>
                             <a href='' onclick="
                                     deleteElement( //Ir a /public/js/messages_methods.js para saber como funciona esta función
                                     '¿Está seguro de querer eliminar el vehiculo {{$vehicle->id}} con las placas {{$vehicle->placas}}?',
                                     'delete_form_{{ $vehicle->id }}');
                                     ">
+
                                 <button type="button" class="btn btn-danger">Eliminar</button>
                             </a>
                         </form>
