ede0b24d558136abf50d09a7a550646bbf950b1c
Erick Guerrero <erickg@valles.udg.mx>
2018-04-18 17:48:48 -0500

    Ya funciona el datepicker, fue porque se volvió a agregar el jquery

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -24,7 +24,7 @@
         <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
         <script type="text/javascript" src="{{ asset('js/message_methods.js') }}"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <!--script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script-->
     </head>
     <body>
         <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
